9495c558e9949a070157597cb5f99a33308d25ae
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-12 20:28:14 +0100

    Add 3-value_argument.js file

---
+++ 3-value_argument.js
@@ -0,0 +1,13 @@
+let message = "";
+
+function printMessage() {
+    const args = process.argv.slice(2);
+    if(args[0] == undefined) {
+        message += "No argument";
+    } else if(args[1] == undefined) {
+        message += args[0];
+    }
+    return message;
+}
+
+console.log(printMessage(""))
