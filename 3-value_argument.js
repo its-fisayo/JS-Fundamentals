9df91cc07a4e1f4d1a27152d4e27bd1e8272a6c6
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-12 20:39:24 +0100

    Update 3-value_argument.js file

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,10 +4,8 @@ function printMessage() {
     const args = process.argv.slice(2);
     if(args[0] == undefined) {
         message += "No argument";
-    } else if(args[1] == undefined) {
+    } else if(args[0] != undefined) {
         message += args[0];
-    } else if(args[2] == undefined) {
-        message += args[0] + " " + args[1];
     }
     return message;
 }
