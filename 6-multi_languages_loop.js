9a54fd51f7ef7ac9cfb483fdabc345ce10170d0f
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-12 21:41:26 +0100

    Update 6-multi_languages_loop.js file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,8 @@
 let myArr = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let message = myArr[0];
-for(let i = 1; i < myArr.length; i++) {
-    message += "\n" + myArr[i]
+let i = 1;
+while( i < myArr.length) {
+    message += "\n" + myArr[i];
+    i++;
 }
 console.log(message);
