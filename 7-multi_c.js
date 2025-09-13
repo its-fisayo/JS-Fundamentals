7c6956fd52df44cdf1479fc5f69712948ab4a1b3
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-13 12:00:08 +0100

    Update 7-multi_c.js file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,17 +1,15 @@
 let message = "";
+const args = process.argv.slice(2);
+let x = parseInt(args[0]);
 
-    const args = process.argv.slice(2);
-    let x = parseInt(args[0]);
-    if(args[0] == undefined) {
-        message += "Missing number of occurrences";
-    } else {
-        for(i = 0; i < x; i++) {
-            if(i == x-1) {
-                message += "C is fun";
-            } else {
-                message += "C is fun\n";
-            }
-        }
+if(args[0] == undefined) {
+    message += "Missing number of occurrences";
+}
+for(i = 0; i < x; i++) {
+    if(i == x-1) {
+        message += "C is fun";
+    } else{
+        message += "C is fun\n";
     }
-    console.log(message);
-
+}
+console.log(message);
