2cb77b0049e25905e803a214a8e89090a674c9c8
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-13 12:30:26 +0100

    Update 6-multi_languages_loop.js file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,13 @@
-let myArr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+/*let myArr = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for(i = 0; i < myArr.length; i++) {
     console.log(myArr[i]);
+}*/
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
 }
