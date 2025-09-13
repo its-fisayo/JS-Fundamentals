4eed04bbf4a22c79f6c6392de43a619306e6774a
David Babatunde <davidbabatundehbc@gmail.com>
2025-09-13 11:47:15 +0100

    Add 7-multi_c.js file

---
+++ 7-multi_c.js
@@ -0,0 +1,20 @@
+let message = "";
+
+function printMessage() {
+    const args = process.argv.slice(2);
+    let x = parseInt(args[0]);
+    if(args[0] == undefined) {
+        message += "Missing number of occurrences";
+    } else {
+        for(i = 0; i < x; i++) {
+            if(i == x-1) {
+                message += "C is fun";
+            } else {
+                message += "C is fun\n";
+            }
+        }
+    }
+    return message;
+}
+
+console.log(printMessage());
